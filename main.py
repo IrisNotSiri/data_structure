a986cd552a86a11660434dfeafc466db302bb48f
Iris Wu <iriswu@irisdeair.scotland.ca>
2020-05-30 23:38:39 -0400

    linked list and binary search tree

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+    # assume data is a string
     def __repr__(self):
         return self.data
+    
 
 class LinkedList:
     def __init__(self):
@@ -46,17 +48,80 @@ ll.add(nodeFirst)
 
 print(ll)
 
-ll.remove("second")
+ll.remove("first")
 print(ll)
 
 
-# from collections import deque
-# llis = deque('abdedc')
-# llis.appendleft('t')
-# llis.popleft()
-# print (llis)
+from collections import deque
+llis = deque('abdedc')
+llis.appendleft('t')
+llis.popleft()
+print (llis)
+
+
+class TreeNode:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+    def __repr__(self):
+      return str(self.data)
+
+class Tree:
+    def __init__(self):
+      self.root = None
+    def insert(self,root,newNode):
+      self.root = root
+      if self.root.data < newNode.data:
+        if self.root.right is None:
+          self.root.right = newNode
+        else:
+          self.insert(self.root.right,newNode)
+      else:
+        if self.root.left is None:
+          self.root.left = newNode
+        else:
+          self.insert(self.root.left, newNode)
+
+
+
+def inorder(root): 
+    if root: 
+        inorder(root.left) 
+        print(root.data) 
+        inorder(root.right) 
+  
+  
+# Driver program to test the above functions 
+# Let us create the following BST 
+#      50 
+#    /      \ 
+#   30     70 
+#   / \    / \ 
+#  20 40  60 80 
+
+
+r = TreeNode(50)
+tree = Tree()
+tree.insert(r,TreeNode(30)) 
+tree.insert(r,TreeNode(20)) 
+tree.insert(r,TreeNode(40)) 
+tree.insert(r,TreeNode(70)) 
+tree.insert(r,TreeNode(60)) 
+tree.insert(r,TreeNode(80)) 
+  
+# Print inoder traversal of the BST 
+inorder(r)
+
+
 
 
         
+
+
+      
+      
+
+
       
 
